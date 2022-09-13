27ab3e8cdaaeadedfa61e8f64b2357e2c8c5b65c
Quan160155 <quantmse160155@fpt.edu.vn>
2022-09-13 11:51:31 +0700

    Update functional components

--- src/components/Players.js
+++ src/components/Players.js
@@ -1,20 +1,19 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { PlayerList } from '../shared/ListOfPlayers'
-export default class Player extends Component {
-    render() {
-        return (
-            <div className='container'>
-                {PlayerList.map((player) => (
-                    <div className='column'>
-                        <div className='card'>
-                            <img src={player.img} />
-                            <h3>{player.name}</h3>
-                            <p className='title'>{player.club}</p>
-                            <p className='detail'><button>Detail</button></p>
-                        </div>
+export default function Players() {
+    return (
+        <div className='container'>
+            {PlayerList.map((player) => (
+                <div className='column'>
+                    <div className='card'>
+                        <img src={player.img} />
+                        <h3>{player.name}</h3>
+                        <p className='title'>{player.club}</p>
+                        <p className='detail'><button>Detail</button></p>
                     </div>
-                ))}
-            </div>
-        )
-    }
+                </div>
+            ))}
+        </div>
+    )
 }
+
